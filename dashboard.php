eb8ff347f728b663f1ad9a00fef96bd8ba3426d7
Ana TSI <aasins@tsiberia.es>
2017-09-20 17:18:59 +0200

    dashboard modificado

--- dashboard.php
+++ dashboard.php
@@ -25,24 +25,27 @@
      //llama a la funcion para devolver el nombre del usuario.
      $nombreuser=$usuario->nombreUsuario($_SESSION['usuario']);
      //saca el nombre del usuario por pantalla.
-      echo "<h3> Bienvenido ".$nombreuser['name']."</h3>";
+      echo "<h3> Bienvenido ".$nombreuser['name']."</h3><br><br>";
 
       //menu
+      //llama a la funcion para saber el menu que mostrar dependiendo de los permisos.
       $menu=$usuario->menuDash($_SESSION['usuario']);
 
+      //dependiendo de los permisos saca el menu correspondiente.
       if ($menu['menu']==1) {
         echo "<a href=''>OPERATIVA</a>";
       }elseif ($menu['menu']==2) {
-        echo "<a href=''>OPERATIVA</a><br><br>";
-        echo "<a href=''>NOMINAS</a><br><br>";
+        echo "<a href=''>OPERATIVA</a>";
+        echo "<a href=''>NOMINAS</a>";
       }elseif ($menu['menu']==3) {
-        echo "<a href=''>OPERATIVA</a><br><br>";
-        echo "<a href=''>NOMINAS</a><br><br>";
-        echo "<a href=''>DIAS LIBRES</a><br><br>";
+        echo "<a href=''>OPERATIVA</a>";
+        echo "<a href=''>NOMINAS</a>";
+        echo "<a href=''>DIAS LIBRES</a>";
       }else {
+        //si el numero de permiso no es correcto, saca un aviso.
         ?>
         <script type="text/javascript">
-          alert('Ups, algo salio mal. Hable con el responsable');
+          alert('Ups, algo salio mal. Hable con el responsable.');
         </script>
         <?php
       }
