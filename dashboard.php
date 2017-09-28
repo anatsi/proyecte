75d4a3919741691a89cc16fb4a09ddb620f6c1b0
Ana TSI <aasins@tsiberia.es>
2017-09-28 18:10:16 +0200

    no message

--- dashboard.php
+++ dashboard.php
@@ -16,6 +16,7 @@
 <html >
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bienvenido.</title>
   <link rel="shortcut icon" href="favicon.ico">
   <link rel="stylesheet" href="css/dashboard.css">
