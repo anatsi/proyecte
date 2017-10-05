ba52a5a59b46a8ba15ecc57fbb5f007de200c09c
Ana TSI <aasins@tsiberia.es>
2017-10-05 16:43:07 +0200

    archivo de servicio.php añadido

---
+++ servicio.php
@@ -0,0 +1,31 @@
+<?php
+//llamamos a la clase de db encarada de la conexion a la base de datos.
+require_once 'db.php';
+/**
+ * Clase servicios encargada de las consultas hacia esta tabla de la db.
+ */
+class Servicio extends db
+{
+  //la clase consruct llama al construct de db, encargado a la conexion a la db.
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+    //funcion para insertar un nuevo servicio en la base de datos.
+  function nuevoServicio($nombre, $recursos, $inicio, $cliente, $responsable, $tel, $correo, $csup, $crrhh, $caf){
+    //realizamos la consuta y la guardamos en $sql
+    $sql="INSERT INTO servicios(id, nombre, recursos, f_inicio, f_fin, id_cliente, responsable, telefono, correo, com_supervisor, com_rrhh, com_admin_fin)
+     VALUES (NULL, '".$nombre."', ".$recursos.", '".$inicio."', NULL, ".$cliente.", '".$responsable."', ".$tel.", '".$correo."', '".$csup."', '".$crrhh."', '".$caf."')";
+    //Realizamos la consulta utilizando la funcion creada en db.php
+    $resultado=$this->realizarConsulta($sql);
+    if($resultado!=false){
+      return true;
+    }else{
+      return null;
+    }
+  }
+}
+
+
+ ?>
