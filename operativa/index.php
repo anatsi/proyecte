850ab048e7c3897886244c860d3398ea1afaf0c4
Ana TSI <aasins@tsiberia.es>
2017-10-06 16:03:44 +0200

    index de operativa hecho

---
+++ operativa/index.php
@@ -0,0 +1,60 @@
+<?php
+//incluimos todas las clases necesarias e iniciamos sus objetos.
+require_once '../sesiones.php';
+require_once '../users.php';
+require_once '../cliente.php';
+
+
+$usuario=new User();
+$sesion=new Sesiones();
+$cliente=new Cliente();
+
+ ?>
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Nuevo servicio</title>
+    <link rel="stylesheet" href="../css/menu.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="../css/formulario.css">
+    <link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="../css/dashboard.css" />
+</head>
+<body>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet'>
+</head>
+
+<div class="codrops-top clearfix">
+  <?php
+    //llamamos a la función para devolver el nombre de usuario.
+    $nombreuser=$usuario->nombreUsuario($_SESSION['usuario']);
+    //sacamos el nombre de usuario por su id
+    echo "<a><strong>Bienvenido ".$nombreuser['name']."</strong></a>";
+   ?>
+  <span class="right"><a href="../logout.php">Cerrar Sesion</a></span>
+</div><!--/ Codrops top bar -->
+
+<div class="site-container">
+  <div class="site-pusher">
+
+    <header class="header">
+
+      <a href="#" class="header__icon" id="header__icon"></a>
+      <a href="../dashboard.php" class="header__logo"><img src="../logo.png" alt=""></a>
+
+      <nav class="menu">
+        <a href="consultarServicio.php">Consultar</a>
+        <a href="nuevoServicio.php">Nuevo Servicio</a>
+      </nav>
+
+    </header>
+
+<!-- Scripts para que el menu en versión movil funcione -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script  src="js/menu.js"></script>
+
+</body>
+</html>
