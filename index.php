50eb9d54588588ab99cfeaa59efb2b1dbd68498c
Ana TSI <aasins@tsiberia.es>
2017-10-19 17:17:17 +0200

    arreglo desastre inicio

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Area trabajadores</title>
 
@@ -22,34 +23,47 @@
 
         <!-- Favicon and touch icons -->
         <link rel="shortcut icon" href="assets/ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
     </head>
     <body>
         <!-- Top content -->
-        <div class="top-content inner-bg container row col-sm-6 col-sm-offset-3 form-box">
-          <div class="form-top">
-              <div class="form-top-left">
-                  <p><img src="assets/files/logo.png" alt="logo TSI" title="Logo TSI" width="100" height="75" /></p>
-                  <h3>Area privada empleados</h3>
-                  <p>Introducir usuario y contraseña para iniciar sesión:</p>
-              </div>
-              <div class="form-top-right">
-                  <i class="fa fa-lock"></i>
-              </div>
-          </div>
-          <div class="form-bottom">
-              <form role="form" action="login.php" method="post" class="login-form">
-                  <div class="form-group">
-                      <label class="sr-only" for="form-username">Usuario...</label>
-                      <input type="text" name="form-username" placeholder="Usuario..." class="form-username form-control" id="form-username">
-                  </div>
-                  <div class="form-group">
-                      <label class="sr-only" for="form-password">Contraseña...</label>
-                      <input type="password" name="form-password" placeholder="Contraseña..." class="form-password form-control" id="form-password">
-                  </div>
-                  <button type="submit" class="btn">Enviar!</button>
-              </form>
-          </div>
-       </div>
+        <div class="top-content">
+
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                            <div class="form-top">
+                                <div class="form-top-left">
+                                    <p><img src="assets/files/logo.png" alt="logo TSI" title="Logo TSI" width="100" height="75" /></p>
+                                    <h3>Area privada empleados</h3>
+                                    <p>Introducir usuario y contraseña para iniciar sesión:</p>
+                                </div>
+                                <div class="form-top-right">
+                                    <i class="fa fa-lock"></i>
+                                </div>
+                            </div>
+                            <div class="form-bottom">
+                                <form role="form" action="login.php" method="post" class="login-form">
+                                    <div class="form-group">
+                                        <label class="sr-only" for="form-username">Usuario...</label>
+                                        <input type="text" name="form-username" placeholder="Usuario..." class="form-username form-control" id="form-username">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="sr-only" for="form-password">Contraseña...</label>
+                                        <input type="password" name="form-password" placeholder="Contraseña..." class="form-password form-control" id="form-password">
+                                    </div>
+                                    <button type="submit" class="btn">Enviar!</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!-- Javascript -->
         <script src="assets/js/jquery-1.11.1.min.js"></script>
         <script src="assets/bootstrap/js/bootstrap.min.js"></script>
