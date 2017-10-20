d3063182f05e394dcb9ec3e56d7e874fca66bb0d
Ana TSI <aasins@tsiberia.es>
2017-10-20 15:50:54 +0200

    tonteriias de dashboard y de index arregladas

--- dashboard.php
+++ dashboard.php
@@ -77,12 +77,6 @@
 				</div>
 			</div>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="js/jquery.dlmenu.js"></script>
-		<script>
-			$(function() {
-				$( '#dl-menu' ).dlmenu();
-			});
-		</script>
 	</body>
 </html>
 <?php } ?>
--- index.php
+++ index.php
@@ -14,10 +14,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Area trabajadores</title>
 
-        <!-- CSS -->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+        <!-- CSS-->
         <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css">
         <link rel="stylesheet" href="assets/css/form-elements.css">
         <link rel="stylesheet" href="assets/css/style.css">
 
